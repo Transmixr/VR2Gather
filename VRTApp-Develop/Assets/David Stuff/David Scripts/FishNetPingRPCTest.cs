b175016beb64d4b3053a0f96f0eb173019ed363a
DavesTUS <d.concannon@research.ait.ie>
2024-10-10 13:23:02 +0100

    Fiddling around with the Fish net Button
    Changed up some stuff with the Fish Net button to try and get it to work in JackVRTFishnet
    
    -Moving around the network observer components
    -Changing how the RPC call is initiated
    
    No joy so far. comitting this so that CWI can take a look if they want

--- VRTApp-Develop/Assets/David Stuff/David Scripts/FishNetPingRPCTest.cs
+++ VRTApp-Develop/Assets/David Stuff/David Scripts/FishNetPingRPCTest.cs
@@ -8,19 +8,23 @@ public class FishNetPingRPCTest : NetworkBehaviour
 {
     [SerializeField]private AudioSource m_AudioSource;
 
+    public void LocalEventFire()
+    {
+        Debug.Log("Firing Event");
+
+        RPCPlaySound();
+    }
+
+
     [ObserversRpc]
     public void RPCPlaySound()
     {
-
-        Debug.Log("Button clicked on Fish Net Object");
-
-        m_AudioSource.Play();
-
         /*Sometimes Fishnet needs RPC events to be checked if the caller is the server so I had this here initially accoriding to normal setup but moved the Play() method out during testing*/
-        //if (IsServerInitialized)
-        //{
-            
-        //}
-        
+        if (IsServerInitialized)
+        {
+            Debug.Log("Button clicked on Fish Net Object");
+
+            m_AudioSource.Play();
+        }
     }
 }
